3c8b4d726e13c1ad5824776e014a6faea0498e24
isysd <ira@gitguild.com>
2017-04-17 20:53:28 -0500

    bin

---
+++ bin/cli.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+var pickCommand = require('../src/main.js').pickCommand
+
+pickCommand(process.argv)
--- src/cli.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-var pickCommand = require('./main.js').pickCommand
-
-pickCommand(process.argv)
